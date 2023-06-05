7dd4e4602e9ec89e3809323df322a04241ab60e5
James Brooks <james.alex.brooks@gmail.com>
2023-06-05 10:06:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+from my_pip_package import __version__
+
+setup(
+    name='marugoto_mods',
+    version=__version__,
+
+    url='https://github.com/brooksja/marugoto_mods.git',
+    author='James Brooks',
+    author_email='jamesalexander.brooks@med.uni-duesseldorf.de',
+
+    py_modules=find_packages(),
+  
+    install_requires=[
+      'https://github.com/KatherLab/marugoto.git',
+    ],
+)
