9dbf88a16cfa7c77de0356ce84298f6d07cc6fe3
James Brooks <james.alex.brooks@gmail.com>
2023-06-22 10:04:49 +0200

    Update mil_mods.py

--- marugoto_mods/mil_mods.py
+++ marugoto_mods/mil_mods.py
@@ -71,7 +71,7 @@ def train(
 
     # build dataloaders
     train_dl = DataLoader(
-        train_ds, batch_size=64, shuffle=True, num_workers=1, drop_last=True
+        train_ds, batch_size=64 if len(train_ds)>64 else 1, shuffle=True, num_workers=1, drop_last=True
     )
     valid_dl = DataLoader(
         valid_ds, batch_size=1, shuffle=False, num_workers=os.cpu_count()
